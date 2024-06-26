3120b96cabeb334c9ed137332ab81425ea75b538
ChrisM345 <chrism.12277@gmail.com>
2024-06-25 20:50:15 -0500

    Added quantity and button to add to cart

--- src/components/ShoppingPage.jsx
+++ src/components/ShoppingPage.jsx
@@ -5,13 +5,37 @@ import "../styles/Shoppingpage.css";
 const ShopPage = () => {
   const [cartItems, setCartItems] = useState([]);
   const [products, setProducts] = useState([]);
+  const [productQty, setProductQty] = useState();
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    console.log("test");
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((json) => setProducts(json));
-  });
+      .then((json) => {
+        setProducts(json);
+        setProductQty(Array(json.length).fill(1));
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-  return (
+  function handleQuantityUpdate(e, index) {
+    let copiedProductQty = [...productQty];
+    console.log(e.target.value);
+    copiedProductQty[index] = e.target.value;
+    setProductQty(copiedProductQty);
+    console.log(productQty);
+  }
+
+  function handleAddItem(e, product, index) {
+    console.log(`Buying ${product} with quantity ${productQty[index]}`);
+  }
+
+  return loading ? (
+    <>
+      <p>Loading</p>
+    </>
+  ) : (
     <>
       <Navigationbar />
       <h1>Shop Page</h1>
@@ -26,6 +50,17 @@ const ShopPage = () => {
               {product.description}
               <br />
               Price: ${product.price}
+              <div className="productButtons">
+                <button onClick={(e) => handleAddItem(e, product.title, product.id - 1)}>Add Item</button>
+                <input
+                  className="product-qty"
+                  type="number"
+                  name="product-quantity"
+                  min="1"
+                  value={productQty[product.id - 1]}
+                  onChange={(e) => handleQuantityUpdate(e, product.id - 1)}
+                ></input>
+              </div>
             </div>
           );
         })}
