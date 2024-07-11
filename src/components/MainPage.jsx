38f327f113b2d6f57762556b0ba58c8f0d70d634
ChrisM345 <chrism.12277@gmail.com>
2024-07-10 19:34:23 -0500

    Added small intro for main page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,7 +1,10 @@
 const MainPage = () => {
   return (
     <>
-      <h1>Welcome to my Shopping Page</h1>
+      <h2>Welcome to my Shopping Page</h2>
+      <p>Sample shop using the FakeStore API.</p>
+      <p>Main focus was using React Router, state, and localStorage.</p>
+      <p>Use the navigation bar to access the shopping page.</p>
     </>
   );
 };
